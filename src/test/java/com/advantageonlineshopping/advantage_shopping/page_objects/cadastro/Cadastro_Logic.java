f9b82952cfc732cadfca6db6065abb580300856a
Luiz Paulo <luizpaulo@MacBook-Air-de-Luiz.local>
2024-01-02 15:02:39 -0300

    Criação do padrão POJO para o armazenamento temporário de valores em tempo de execução

--- src/test/java/com/advantageonlineshopping/advantage_shopping/page_objects/cadastro/Cadastro_Logic.java
+++ src/test/java/com/advantageonlineshopping/advantage_shopping/page_objects/cadastro/Cadastro_Logic.java
@@ -1,16 +1,20 @@
 package com.advantageonlineshopping.advantage_shopping.page_objects.cadastro;
 
+import static org.junit.Assert.assertTrue;
 import com.advantageonlineshopping.advantage_shopping.utils.RandomUtils;
+import com.advantageonlineshopping.advantage_shopping.utils.TemporaryTestData;
 import com.advantageonlineshopping.advantage_shopping.utils.WebActions;
 
 public class Cadastro_Logic extends WebActions {
 
 	private CadastroPage cadastroPage;
 	private RandomUtils randomUtils;
+	private TemporaryTestData temporaryTestData;
 
 	public Cadastro_Logic() {
 		cadastroPage = new CadastroPage();
 		randomUtils = new RandomUtils();
+		temporaryTestData = new TemporaryTestData();
 	}
 
 	public void clickOnMenuUser() {
@@ -25,8 +29,9 @@ public class Cadastro_Logic extends WebActions {
 
 	public void insertUserName() {
 		waitUntilElementToBeClickable(cadastroPage.getCmpUserName());
-		String userRandomico = "test " + randomUtils.geraStringRandomica(6);
-		writeText(userRandomico, cadastroPage.getCmpUserName());
+		String randomicUser = "test" + randomUtils.generateRandomString(6);
+		writeText(randomicUser, cadastroPage.getCmpUserName());
+		temporaryTestData.setUser(randomicUser);
 	}
 
 	public void insertEmailAdress(String email) {
@@ -36,14 +41,14 @@ public class Cadastro_Logic extends WebActions {
 
 	public void insertPassword() {
 		waitUntilElementToBeClickable(cadastroPage.getCmpPassWord());
-		String randomicPassword = randomUtils.geraStringRandomica(6);
+		String randomicPassword = "P12" + randomUtils.generateRandomString(6);
 		writeText(randomicPassword, cadastroPage.getCmpPassWord());
+		temporaryTestData.setPassword(randomicPassword);
 	}
 
 	public void confirmPassword() {
 		waitUntilElementToBeClickable(cadastroPage.getCmpConfirmPassWord());
-		String randomicPassword = randomUtils.geraStringRandomica(6);
-		writeText(randomicPassword, cadastroPage.getCmpConfirmPassWord());
+		writeText(temporaryTestData.getPassword(), cadastroPage.getCmpConfirmPassWord());
 	}
 
 	public void insertFirstName(String fName) {
@@ -89,5 +94,26 @@ public class Cadastro_Logic extends WebActions {
 	public void selectUseConditions() {
 		waitUntilElementToBeClickable(cadastroPage.getBxAgree());
 		click(cadastroPage.getBxAgree());
+
+	}
+
+	public void clickOnRegister() {
+		waitUntilElementToBeClickable(cadastroPage.getBtnRegister());
+		click(cadastroPage.getBtnRegister());
+	}
+
+	public void validateUserCreation() {
+		waitForDesiredElementPresence(cadastroPage.getUserNameIcon());
+
+		String user = driver.findElement(cadastroPage.getUserNameIcon()).getText();
+		String userData = temporaryTestData.getUser();
+
+		if (!user.isEmpty() && user.contains(userData)) {
+			System.out.println("User was created successfully.");
+			assertTrue(true);
+		} else {
+			System.out.println("The message of created user is either empty or doesn't match the expected data.");
+			assertTrue(false);
+		}
 	}
 }
